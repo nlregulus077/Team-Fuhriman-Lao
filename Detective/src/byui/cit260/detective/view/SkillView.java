127a6da121021c531a0bcb4414ad90deaf6eb9df
Jeanina Lao <Nina@10.18.148.144>
2016-06-15 10:20:11 -0600

    Skill View

--- Detective/src/byui/cit260/detective/view/SkillView.java
+++ Detective/src/byui/cit260/detective/view/SkillView.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * I don't recommend implementing inheritance here.
  */
 package byui.cit260.detective.view;
 
