a0fc96e97e7d331c2cac3a086170ab2eeb74443c
Jeanina Lao <Nina@10.18.148.144>
2016-06-16 10:02:02 -0600

    6/16/2016 10:01 - NotepadView added

---
+++ Detective/src/byui/cit260/detective/view/NotepadView.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.detective.view;
+
+/**
+ *
+ * @author Nina
+ */
+public class NotepadView extends View {
+
+    public NotepadView() {
+        super("\n"
+                  + "\n ---------------------------------------------------"
+                  + "\n | Notepad                                          |"
+                  + "\n ---------------------------------------------------"
+                  + "\n\tV - View Notes"
+                  + "\n\tA - Add Notes"
+                  + "\n\tQ - Quit"
+                  + "\n----------------------------------------------------");
+    }   
+    
+    @Override
+     public boolean doAction(String choice) {
+        
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "V":
+                this.viewNotes();
+                break;
+            case "K":
+                this.addNotes();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+    
+}
+        return false;
+    }
+
+    private void viewNotes() {
+        System.out.println("*** viewNotes() stub function called. ***");
+    }
+
+    private void addNotes() {
+        System.out.println("*** addNotes() stub function called. ***");
+    }
+}
